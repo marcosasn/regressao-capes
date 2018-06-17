c39c8bd4f1effd98c245c3ef34e80eb9ac0a8578
marcosasn <marcos.nascimento@ccc.ufcg.edu.br>
2018-06-16 22:16:29 -0300

    Correções finais.

--- code/lib.R
+++ code/lib.R
@@ -26,16 +26,15 @@ import_data <- function(){
     cacc_raw = read_csv("https://raw.githubusercontent.com/nazareno/ciencia-de-dados-1/master/3-Agrupamento-e-PCA/dados/capes-cacc.csv", col_types = "") %>% 
         mutate_all(funs(replace(., is.na(.), 0))) %>% 
         filter(Teses + Dissertacoes > 0)
-    
     cacc_raw %>% 
         write_csv(here::here("data/cacc.csv"))
 }
 
 read_projectdata <- function(){
-    readr::read_csv(here::here("data/cacc.csv"), 
+    readr::read_csv(here::here("data/cacc.csv"),
                     col_types = cols(
                         .default = col_integer(),
-                        Instituicao = col_character(),
+                        `Instituição` = col_character(),
                         Programa = col_character(),
                         Sigla = col_character(),
                         `Tem doutorado` = col_character(),
@@ -43,4 +42,10 @@ read_projectdata <- function(){
                         `Docentes permanentes` = col_double(),
                         `Docentes visitantes` = col_double()
                     ))
+}
+
+normalize_column <- function(cacc){
+    cacc$`Tem doutorado` = gsub("SIM","Sim",cacc$`Tem doutorado`)
+    
+    return(cacc)
 }
